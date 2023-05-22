9258d531fe88abc146c6c4aefa957c31eb180098
swaraj-jain <56780022+swaraj-jain@users.noreply.github.com>
2023-05-22 16:35:26 +0530

    Update blogControllers.js

--- controllers/blogControllers.js
+++ controllers/blogControllers.js
@@ -3,10 +3,10 @@ const Blog   = require("../models/blog.js");
 
 const getAllBlog = async (req, res) => {
 
-    const all_blog = await Blog.find({});
+    const allBlog = await Blog.find({});
     res.status(200).json({
         "status": "success",
-        "data": all_blog
+        "data": allBlog
     })
    
 }
